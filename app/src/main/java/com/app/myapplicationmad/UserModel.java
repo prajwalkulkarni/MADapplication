d0af53ec836f27c83a1d3323dff86ca781ab6cf4
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-01-12 10:56:31 +0530

    MAD lab exam

---
+++ app/src/main/java/com/app/myapplicationmad/UserModel.java
@@ -0,0 +1,34 @@
+package com.app.myapplicationmad;
+
+import java.io.Serializable;
+
+
+public class UserModel implements Serializable{
+
+    private String name, hobby;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
